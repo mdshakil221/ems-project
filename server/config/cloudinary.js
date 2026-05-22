3412bec8bf937c834fce95fa57eff068ce4fda24
md shakil <mdshakil6539@gmail.com>
2026-05-22 21:47:05 +0600

    fix: remove dotenv from cloudinary config

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
