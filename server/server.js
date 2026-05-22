13cc51cc96ac19f6fe9fa97997ad1abaef0de9c9
md shakil <mdshakil6539@gmail.com>
2026-05-22 21:40:46 +0600

    test: cloudinary env check

--- server/server.js
+++ server/server.js
@@ -65,6 +65,18 @@ app.use("/api/announcements", announcementRoutes);
 app.use("/api/holidays", holidayRoutes);
 app.use("/api/documents", documentRoutes);
 
+
+
+
+app.get("/test-cloudinary", (req, res) => {
+  res.json({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    has_secret: !!process.env.CLOUDINARY_API_SECRET
+  });
+});
+
+
 app.get("/", (req, res) => {
   res.json({ message: "EMS Server চলছে! 🚀" });
 });
